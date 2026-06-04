7a1e0e1e6a5a7a55c02292d95e8a96e8e838e1e0
Bernhard Häussner <bxt@die-optimisten.net>
2026-06-04 14:12:10 +0200

    feat: group expenses by day (not actually sure if I like this feature)

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -4,9 +4,16 @@
   import NumberInput from "$lib/NumberInput/NumberInput.svelte";
   import { getActiveWallet, addExpense } from "$lib/persistence.svelte";
   import RelativeTime from "$lib/RelativeTime/RelativeTime.svelte";
+  import { SvelteDate } from "svelte/reactivity";
   import { slide } from "svelte/transition";
 
   let amount = $state(0);
+
+  let expenseDays = $derived(
+    Object.groupBy(getActiveWallet().expenses.toReversed(), ({ date }) =>
+      date.substring(0, 10),
+    ),
+  );
 </script>
 
 <div
@@ -52,10 +59,31 @@
   </div>
 </form>
 <ul class="flex flex-col gap-1">
-  {#each getActiveWallet().expenses.toReversed() as expense (expense.id)}
-    <li transition:slide class="flex items-baseline gap-2">
-      <Amount value={expense.amount} class="flex-1 text-2xl" />
-      <RelativeTime date={expense.date} class="flex-1 text-gray-500" />
+  {#each Object.entries(expenseDays) as [expenseDay, expenses] (expenseDay)}
+    <li transition:slide>
+      {#if expenseDay !== new SvelteDate().toISOString().substring(0, 10)}
+        <div
+          class="text-gray-500 text-center border-b border-gray-300 mbe-3 -mbs-3"
+        >
+          <span class=" bg-white -mb-2 relative top-3 px-3">
+            {#if expenseDay === new Date(+new Date() - 24 * 60 * 60 * 1000)
+                .toISOString()
+                .substring(0, 10)}
+              yesterday
+            {:else}
+              {expenseDay}
+            {/if}</span
+          >
+        </div>
+      {/if}
+      <ul class="flex flex-col gap-1">
+        {#each expenses as expense (expense.id)}
+          <li transition:slide class="flex items-baseline gap-2">
+            <Amount value={expense.amount} class="flex-1 text-2xl" />
+            <RelativeTime date={expense.date} class="flex-1 text-gray-500" />
+          </li>
+        {/each}
+      </ul>
     </li>
   {/each}
 </ul>
